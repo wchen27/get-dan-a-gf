8b2e02c1fd325ab9bae668ca71969cbc812cfd9e
Wilson Chen <wilson0109@outlook.com>
2022-04-16 15:57:13 -0400

    updated website

--- Website/website_main.py
+++ Website/website_main.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
+
+@app.route('/login-page.html/')
+def login():
+    return render_template('login-page.html')
 
 if "__name__" == "__main__":
     app.run(debug=True)
